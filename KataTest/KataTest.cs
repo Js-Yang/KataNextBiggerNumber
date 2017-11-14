1e557dac4585d890bb93371ac6591791ce7b5282
jason.yang <chyang590@gmail.com>
2017-11-14 23:30:32 +0800

    add test case NextBiggerNumber_When_Input_Is_21_Then_return_Minus1 and the test case Failed

--- KataTest/KataTest.cs
+++ KataTest/KataTest.cs
@@ -7,6 +7,7 @@ namespace KataBiggerNumberTests
     {
         [TestCase(9, -1, TestName = "NextBiggerNumber_When_Input_Is_9_Then_return_Minus1")]
         [TestCase(11, -1, TestName = "NextBiggerNumber_When_Input_Is_11_Then_return_Minus1")]
+        [TestCase(21, -1, TestName = "NextBiggerNumber_When_Input_Is_21_Then_return_Minus1")]
         public void NextBiggerNumber_Test(int input, int expected)
         {
             var result = Kata.NextBiggerNumber(input);
