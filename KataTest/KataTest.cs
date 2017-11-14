813f74a09bcc0d6b93c87a4af4dd6c49ab097af3
jason.yang <chyang590@gmail.com>
2017-11-14 23:09:32 +0800

    refacotor test case

--- KataTest/KataTest.cs
+++ KataTest/KataTest.cs
@@ -5,27 +5,13 @@ namespace KataBiggerNumberTests
 {
     public class KataTest
     {
-        [Test]
-        public void NextBiggerNumber_When_Input_Is_9_Then_return_Minus1()
+        [TestCase(9, -1, TestName = "NextBiggerNumber_When_Input_Is_9_Then_return_Minus1")]
+        [TestCase(11, -1, TestName = "NextBiggerNumber_When_Input_Is_11_Then_return_Minus1")]
+        public void NextBiggerNumber_Test(int input, int expected)
         {
-            var input = 9;
-
             var result = Kata.NextBiggerNumber(input);
 
-            var expected = -1;
             Assert.AreEqual(expected, result);
         }
-
-        [Test]
-        public void NextBiggerNumber_When_Input_Is_11_Then_return_Minus1()
-        {
-            var input = 11;
-
-            var result = Kata.NextBiggerNumber(input);
-
-            var expected = -1;
-            Assert.AreEqual(expected, result);
-        }
-
     }
 }
