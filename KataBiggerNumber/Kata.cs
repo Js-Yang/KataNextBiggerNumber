56773f09f8ae71b0049d2288392881ca10f2f463
jason.yang <chyang590@gmail.com>
2017-11-14 23:07:53 +0800

    let test case NextBiggerNumber_When_Input_Is_11_Then_return_Minus1 PASS

--- KataBiggerNumber/Kata.cs
+++ KataBiggerNumber/Kata.cs
@@ -4,7 +4,7 @@
     {
         public static double NextBiggerNumber(int input)
         {
-            if (input < 10)
+            if (input < 10 || (input / 10 % 10) == (input / 1 % 10))
             {
                 return -1;
             }
