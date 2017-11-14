42fd33f124368285cb8f7386514f29a7d26b147e
jason.yang <chyang590@gmail.com>
2017-11-14 23:36:18 +0800

    rename test case name

--- KataTest/KataTest.cs
+++ KataTest/KataTest.cs
@@ -5,9 +5,9 @@ namespace KataBiggerNumberTests
 {
     public class KataTest
     {
-        [TestCase(9, -1, TestName = "NextBiggerNumber_When_Input_Is_9_Then_return_Minus1")]
-        [TestCase(11, -1, TestName = "NextBiggerNumber_When_Input_Is_11_Then_return_Minus1")]
-        [TestCase(21, -1, TestName = "NextBiggerNumber_When_Input_Is_21_Then_return_Minus1")]
+        [TestCase(9, -1, TestName = "NextBiggerNumber When Input Is 9 Then return -1")]
+        [TestCase(11, -1, TestName = "NextBiggerNumber When Input Is 11 Then return Minus1")]
+        [TestCase(21, -1, TestName = "NextBiggerNumber When Input Is 21 Then return Minus1")]
         public void NextBiggerNumber_Test(int input, int expected)
         {
             var result = Kata.NextBiggerNumber(input);
