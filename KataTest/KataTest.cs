c025191461e6005982cb02b848ebe2a073bbd39c
jason.yang <chyang590@gmail.com>
2017-11-14 20:30:30 +0800

    add test case NextBiggerNumber_When_Input_Is_111_Then_return_Minus1, and the test case failed

--- KataTest/KataTest.cs
+++ KataTest/KataTest.cs
@@ -16,5 +16,16 @@ namespace KataBiggerNumberTests
             Assert.AreEqual(expected, result);
         }
 
+        [Test]
+        public void NextBiggerNumber_When_Input_Is_111_Then_return_Minus1()
+        {
+            var input = 111;
+
+            var result = Kata.NextBiggerNumber(input);
+
+            var expected = -1;
+            Assert.AreEqual(expected, result);
+        }
+
     }
 }
