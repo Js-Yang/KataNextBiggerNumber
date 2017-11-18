7cc370a946f935f236533b7e505d1699da9ad87d
jason.yang <chyang590@gmail.com>
2017-11-18 23:30:51 +0800

    add test case "NextBiggerNumber When Input Is 1234567890 Then return 1234567908", and it failed

--- KataBiggerNumber/Kata.cs
+++ KataBiggerNumber/Kata.cs
@@ -2,48 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace KataBiggerNumber
+public class Kata
 {
-    public class Kata
+    public static long NextBiggerNumber(long input)
     {
-        public static long NextBiggerNumber(long input)
+        if (NoMoreBiggerNumber(input))
         {
-            if (NoMoreBiggerNumber(input))
+            return -1;
+        }
+
+        return GetNextBiggerNumberBy(input);
+    }
+
+    private static bool NoMoreBiggerNumber(long input)
+    {
+        var numbers = input.ToString();
+        return numbers.Distinct().Count() == 1 || string.Concat(numbers.OrderByDescending(charactor => charactor)) == numbers;
+    }
+
+    private static long GetNextBiggerNumberBy(long input)
+    {
+        var numbers = input.ToString().ToArray();
+
+        for (var index = numbers.Length - 1; index > 0; index--)
+        {
+            var nextIndex = index - 1;
+            if (numbers[index] > numbers[nextIndex])
             {
-                return -1;
+                Swap(numbers, index, nextIndex);
+                break;
             }
-
-            return GetNextBiggerNumberBy(input);
         }
 
-        private static bool NoMoreBiggerNumber(long input)
-        {
-            var numbers = input.ToString();
-            return numbers.Distinct().Count() == 1 || string.Concat(numbers.OrderByDescending(charactor => charactor)) == numbers;
-        }
+        return Convert.ToInt64(string.Concat(numbers.ToArray()));
+    }
 
-        private static long GetNextBiggerNumberBy(long input)
-        {
-            var numbers = input.ToString().ToArray();
-            
-            for (var index = numbers.Length - 1; index > 0; index--)
-            {
-                var nextIndex = index - 1;
-                if (numbers[index] > numbers[nextIndex])
-                {
-                    Swap(numbers, index, nextIndex);
-                    break;
-                }
-            }
-
-            return Convert.ToInt64(string.Concat(numbers.ToArray()));
-        }
-
-        private static void Swap(IList<char> numbers, int i, int nextIndex)
-        {
-            var temp = numbers[i];
-            numbers[i] = numbers[nextIndex];
-            numbers[nextIndex] = temp;
-        }
+    private static void Swap(IList<char> numbers, int i, int nextIndex)
+    {
+        var temp = numbers[i];
+        numbers[i] = numbers[nextIndex];
+        numbers[nextIndex] = temp;
     }
 }
--- KataTest/KataTest.cs
+++ KataTest/KataTest.cs
@@ -1,5 +1,4 @@
-﻿using KataBiggerNumber;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace KataBiggerNumberTests
 {
@@ -11,6 +10,8 @@ namespace KataBiggerNumberTests
         [TestCase(12, 21, TestName = "NextBiggerNumber When Input Is 12 Then return 21")]
         [TestCase(513, 531, TestName = "NextBiggerNumber When Input Is 513 Then return 531")]
         [TestCase(414, 441, TestName = "NextBiggerNumber When Input Is 414 Then return 441")]
+        [TestCase(144, 414, TestName = "NextBiggerNumber When Input Is 144 Then return 414")]
+        [TestCase(1234567890, 1234567908, TestName = "NextBiggerNumber When Input Is 1234567890 Then return 1234567908")]
         public void NextBiggerNumber_Test(long input, long expected)
         {
             var result = Kata.NextBiggerNumber(input);
