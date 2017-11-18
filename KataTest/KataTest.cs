0b4a6cf42ca018ac552a2607556dac67120d6ea1
jason.yang <chyang590@gmail.com>
2017-11-18 23:25:02 +0800

    test case design wrong, should be "NextBiggerNumber When Input Is 414 Then return 441", and after chagne, the test case pass

--- KataTest/KataTest.cs
+++ KataTest/KataTest.cs
@@ -10,7 +10,7 @@ namespace KataBiggerNumberTests
         [TestCase(21, -1, TestName = "NextBiggerNumber When Input Is 21 Then return -1")]
         [TestCase(12, 21, TestName = "NextBiggerNumber When Input Is 12 Then return 21")]
         [TestCase(513, 531, TestName = "NextBiggerNumber When Input Is 513 Then return 531")]
-        [TestCase(441, 414, TestName = "NextBiggerNumber When Input Is 441 Then return 414")]
+        [TestCase(414, 441, TestName = "NextBiggerNumber When Input Is 414 Then return 441")]
         public void NextBiggerNumber_Test(long input, long expected)
         {
             var result = Kata.NextBiggerNumber(input);
