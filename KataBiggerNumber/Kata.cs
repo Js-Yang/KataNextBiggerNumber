60ec5159c67f5024c6760d6e3457f2c4f0cbdf91
jason.yang <chyang590@gmail.com>
2017-11-19 19:27:00 +0800

    rename variable

--- KataBiggerNumber/Kata.cs
+++ KataBiggerNumber/Kata.cs
@@ -13,28 +13,40 @@ public class Kata
 
         var numbers = input.ToString().ToArray();
         var splitPosition = GetSplitPositionBy(numbers);
-        SortNumberFrom(splitPosition, numbers);
+        SortNumbersFrom(splitPosition, numbers);
         Swap(splitPosition, FindBiggerNumberFrom(splitPosition, numbers), numbers);
 
         return Convert.ToInt64(string.Concat(numbers.ToArray()));
     }
 
-    private static void SortNumberFrom(int splitNumber, char[] numbers)
+    private static void SortNumbersFrom(int index, char[] numbers)
     {
-        var partialNumber = splitNumber + 1;
-        GetParialNumbersFrom(partialNumber, numbers).OrderBy(num => num).ToList().CopyTo(numbers, partialNumber);
+        var partialPostion = index + 1;
+        GetParialNumbersFrom(partialPostion, numbers).OrderBy(num => num).ToList().CopyTo(numbers, partialPostion);
     }
 
-    private static int FindBiggerNumberFrom(int current, char[] numbers)
+    private static int FindBiggerNumberFrom(int index, char[] numbers)
     {
-        var firstPartialIndex = current + 1;
-        return numbers.ToList().FindIndex(firstPartialIndex, number => number > numbers[current]);
+        var startIndex = index + 1;
+        return numbers.ToList().FindIndex(startIndex, number => number > numbers[index]);
     }
 
     private static bool NoMoreBiggerNumber(long input)
     {
         var numbers = input.ToString();
         return numbers.Distinct().Count() == 1 || string.Concat(numbers.OrderByDescending(charactor => charactor)) == numbers;
+    }
+
+    private static IEnumerable<char> GetParialNumbersFrom(int index, char[] numbers)
+    {
+        return numbers.Skip(index).Take(numbers.Length - index);
+    }
+
+    private static void Swap(int index, int nextIndex, IList<char> numbers)
+    {
+        var temp = numbers[index];
+        numbers[index] = numbers[nextIndex];
+        numbers[nextIndex] = temp;
     }
 
     private static int GetSplitPositionBy(char[] numbers)
@@ -49,17 +61,5 @@ public class Kata
             }
         }
         return splitPosition;
-    }
-
-    private static IEnumerable<char> GetParialNumbersFrom(int index, char[] numbers)
-    {
-        return numbers.Skip(index).Take(numbers.Length - index);
-    }
-
-    private static void Swap(int index, int nextIndex, IList<char> numbers)
-    {
-        var temp = numbers[index];
-        numbers[index] = numbers[nextIndex];
-        numbers[nextIndex] = temp;
     }
 }
