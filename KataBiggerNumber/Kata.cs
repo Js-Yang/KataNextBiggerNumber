019eddfaa71023423689d2f97728bc947b6380c1
jason.yang <chyang590@gmail.com>
2017-11-19 17:13:58 +0800

    refactor: extract method and use findIndex and copyTo instead of for loop

--- KataBiggerNumber/Kata.cs
+++ KataBiggerNumber/Kata.cs
@@ -25,27 +25,25 @@ public class Kata
     private static long GetNextBiggerNumberBy(long input)
     {
         var numbers = input.ToString().ToArray();
-        
-        for (var index = numbers.Length - 1; index > 0; index--)
+
+        for (var current = numbers.Length - 1; current > 0; current--)
         {
-            var nextIndex = index - 1;
-            if (numbers[index] > numbers[nextIndex])
+            var priviousOfCurrent = current - 1;
+            if (numbers[current] > numbers[priviousOfCurrent])
             {
-                var subNumbers = numbers.Skip(index).Take(numbers.Length - index).OrderBy(x => x).ToArray();
-                Array.Copy(subNumbers, 0, numbers, index, subNumbers.Length);
-                for (int i = 0; i < subNumbers.Length; i++)
-                {
-                    if (numbers[nextIndex] < subNumbers[i])
-                    {
-                        Swap(numbers, nextIndex, i + index);
-                        break;
-                    }
-                }
+                Swap(numbers, priviousOfCurrent, IndexOfNextNumber(numbers, current));
                 break;
             }
         }
 
         return Convert.ToInt64(string.Concat(numbers.ToArray()));
+    }
+
+    private static int IndexOfNextNumber(char[] numbers, int current)
+    {
+        var subNumbers = numbers.Skip(current).Take(numbers.Length - current).OrderBy(x => x).ToList();
+        subNumbers.CopyTo(numbers, current);
+        return subNumbers.FindIndex(x => x > numbers[current - 1]) + current;
     }
 
     private static void Swap(IList<char> numbers, int i, int nextIndex)
